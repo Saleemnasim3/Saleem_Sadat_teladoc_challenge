d1e6271164cb84302971f7b96e33c48d6b3b66df
Saleemsadat <80433912+Saleemsadat@users.noreply.github.com>
2021-10-29 02:58:04 -0400

    Saleem_Sadat_teladoc_challenge::Completed

---
+++ src/test/java/Runner/SmokeRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/Resources/Features",
+        glue = "Steps",
+        dryRun = false,
+        monochrome = true,
+        strict = true,
+        tags = ""
+
+
+)
+
+public class SmokeRunner {
+}
